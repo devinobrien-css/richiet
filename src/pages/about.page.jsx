b023e82b6a2b63b216eb7b7e14911e4208c4fda5
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2023-01-04 09:33:56 -0500

    updates

---
+++ src/pages/about.page.jsx
@@ -0,0 +1,159 @@
+import { Icon } from "@iconify/react";
+import gsap from "gsap";
+import { useLayoutEffect, useState } from "react";
+
+const events = [
+    {
+      title:'Playing on the Porch',
+      location:''
+    },
+    {
+      title:'Swingbellies',
+      location:''
+    },
+    {
+      title:'The Bitter End',
+      location:''
+    },
+    {
+      title:'Terra Blues',
+      location:''
+    },
+    {
+      title:'event4'
+    },
+    {
+      title:'event5'
+    },
+    {}
+  ]
+  
+  
+  const Carousel = () => {
+    const [current,setIndex] = useState(0)
+    
+    return (
+      <div className="relative">
+        <div 
+          className="text-white absolute border top-1/2" 
+          onClick={()=>{
+            if(current >= 0)
+              setIndex(current-1)
+          }}
+        >
+          <Icon icon="material-symbols:arrow-back-ios-new-rounded" />
+        </div>
+        <div 
+          className="text-white absolute border top-1/2 right-0"
+          onClick={()=>{
+            if(current<events.length-3)
+              setIndex(current+1)
+          }}
+        >
+          <Icon icon="material-symbols:arrow-forward-ios-rounded" />
+        </div>
+        
+        <div className="w-4/5 my-8 flex overflow-hidden no-wrap mx-auto">
+          {
+            events.map((event,index) => {
+              const translate =  `-translate-x-[${current*100}%]`
+  
+              return (
+                <div key={index} className={`transition-all transform-gpu border rounded h-32 flex items-center w-1/3 scale-75 my-auto shrink-0 ${translate} ${index===current+1?'scale-100':'scale-75'} ${index===events.length-1?'hidden':''}`}>
+                  <p className="text-white mx-auto">{event.title}</p>
+                </div>      
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+}
+
+const AboutPhoto = () => {
+    useLayoutEffect(() => {
+      gsap.fromTo("#circle-image",{
+        opacity:0.8
+      },{
+        opacity:0.3,
+        duration:4,
+        repeat:-1,
+        yoyo:true
+      })
+    },[])
+  
+    return (
+      <svg width="850" height="809" viewBox="0 0 850 889" fill="none">
+        <ellipse cx="456.5" cy="259" rx="258.5" ry="259" fill="#F95C75" fill-opacity="0.98"/>
+        <circle cx="245.5" cy="352.5" r="245.5" fill="#EDC1C8" fill-opacity="0.98"/>
+        <circle cx="745.5" cy="798.5" r="52.5" fill="#EDC1C8" fill-opacity="0.98"/>
+        <circle cx="599.5" cy="806.5" r="52.5" fill="#F95C75" fill-opacity="0.98"/>
+        <ellipse cx="812" cy="723.5" rx="31" ry="30.5" fill="#EDC1C8" fill-opacity="0.98"/>
+        <ellipse cx="831" cy="624" rx="19" ry="21" fill="#EDC1C8" fill-opacity="0.98"/>
+        <ellipse cx="715" cy="687.5" rx="31" ry="30.5" fill="#EDC1C8" fill-opacity="0.98"/>
+        <ellipse cx="460" cy="851.5" rx="31" ry="30.5" fill="#EDC1C8" fill-opacity="0.98"/>
+        <ellipse cx="630" cy="858.5" rx="31" ry="30.5" fill="#EDC1C8" fill-opacity="0.98"/>
+        <ellipse cx="806" cy="621.5" rx="25" ry="23.5" fill="#F95C75" fill-opacity="0.98"/>
+        <ellipse cx="709" cy="710.5" rx="25" ry="23.5" fill="#F95C75" fill-opacity="0.98"/>
+        <rect x="106" y="107" width="700" height="699" rx="349.5" fill="url(#pattern0)"/>
+  
+  
+        <g clipPath="url(#circle-clip)" transform="translate(0 150)" >
+          <image href="./img/main.jpg" width="1186" height="697" filter="url(#discrete)" />
+        </g>
+        <g clipPath="url(#circle-clip)" transform="translate(0 150)" >
+          <image href="./img/main.jpg" width="1186" height="697" id="circle-image" />
+        </g>
+        <circle x="0" y="0" width="1000" height="1000" fill="url(#image)"/>
+        <defs>
+          <filter id="gamma">
+              <feComponentTransfer>
+                  <feFuncR type="gamma" amplitude="2" exponent="1"/>
+                  <feFuncG type="gamma" amplitude="2" exponent="3"/>
+                  <feFuncB type="gamma" amplitude="2" exponent="1"/>
+              </feComponentTransfer>
+          </filter>
+          <filter id="discrete">
+            <feComponentTransfer>
+              <feFuncR type="discrete" tableValues="0 .5 1 1"/>
+              <feFuncG type="discrete" tableValues="0  5 1"/>
+              <feFuncB type="discrete" tableValues="0 .5 1 1"/>
+            </feComponentTransfer>
+          </filter>
+          <circle id="circle" cx="450" cy="350" r="350" vector-effect="non-scaling-stroke"/>
+          <clipPath id="circle-clip">
+            <use href="#circle"/>
+          </clipPath>
+        </defs>
+      </svg>
+  
+    )
+}
+
+export const About = () => {
+
+    return (
+        <div className="md:flex">
+          <div className="p-8 mt-24 md:w-7/12">
+            <div>
+              <p className="text-white text-7xl font-light">This months</p>
+              <p className="text-white text-7xl ">Crowd Gathering gigs !</p>
+
+              <p className="text-gray-500 py-4">
+                Lorem ipsum dolor, sit amet consectetur 
+                adipisicing elit. Molestias nostrum veniam 
+                eaque molestiae, eligendi quod nam voluptate 
+                velit quaerat corporis!
+              </p>
+
+              <div className="">
+                <button className="rounded-3xl bg-clr-red text-white text-lg py-2 px-8">Listen Now</button>
+                <button className="rounded-3xl bg-clr-red text-white text-lg py-2 px-8 ml-8">What's Next</button>
+              </div>
+            </div>
+            <Carousel />
+          </div>
+          <AboutPhoto />
+        </div>
+    )
+}
---
+++ src/pages/gigs.page.jsx
@@ -0,0 +1,13 @@
+import { Icon } from "@iconify/react";
+import gsap from "gsap";
+import { useLayoutEffect, useState } from "react";
+
+export const Gigs = () => {
+    return (
+        <div>
+          <div>
+            <p className="text-white text-7xl font-light">Upcoming Gigs</p>
+          </div>
+        </div>
+    )
+}
